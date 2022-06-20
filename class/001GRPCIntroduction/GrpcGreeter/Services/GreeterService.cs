537aaa0face82a2b80fe15aa8668260ff3030b40
Diego Goytia <diego.canizales1995@gmail.com>
2022-06-20 15:50:18 -0500

    Day 42/100: ASP.NET Core starters with gRPC.

---
+++ class/001GRPCIntroduction/GrpcGreeter/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using GrpcGreeter;
+
+namespace GrpcGreeter.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<GreetingReply> SayHello(GreetingRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new GreetingReply
+        {
+            Message = String.Format("Hello {0} {1}", request.Greeting.FirstName, request.Greeting.LastName)
+        });
+    }
+}
+
---
+++ class/001GRPCIntroduction/GrpcGreeterClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5252");
+
+await channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("Client connected successfully");
+});
+
+
+var client = new Greeter.GreeterClient(channel);
+
+DoSimpleGreet(client, "Diego", "Canizales");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
+async static void DoSimpleGreet(Greeter.GreeterClient client, String firstName, String lastName )
+{
+    var greeting = new Greeting
+    {
+        FirstName = firstName,
+        LastName = lastName
+    };
+    var reply = await client.SayHelloAsync(
+    new GreetingRequest
+    {
+        Greeting = greeting
+    });
+    Console.WriteLine("Greeting: " + reply.Message);
+}
