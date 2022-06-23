2952242a81c3aa9577e409b0a85f9c53c969b17d
Diego Goytia <diego.canizales1995@gmail.com>
2022-06-22 19:36:42 -0500

    Adding beginning of ServerStreaming exercise.

---
+++ exercises/PrimeNumberDecomposition/PrimeNumberDecomposer/Services/GreeterService.cs
@@ -0,0 +1,41 @@
+using Grpc.Core;
+using GRPCPrimeNumberDecomposer;
+
+namespace GRPCPrimeNumberDecomposer.Services;
+
+public class PrimeNumberDecomposerService : PrimeNumberDecomposer.PrimeNumberDecomposerBase
+{
+    private readonly ILogger<PrimeNumberDecomposerService> _logger;
+    public PrimeNumberDecomposerService(ILogger<PrimeNumberDecomposerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task PrimeNumberDecompose(
+        PrimeNumberDecompositionRequest request,
+        IServerStreamWriter<PrimeNumberDecompositionResponse> responseStream,
+        ServerCallContext context )
+    {
+
+        int k = 2;
+        int N = Int32.Parse(request.Number);
+
+        while (N > 1)
+        {
+            if (N % k == 0)
+            {
+                await responseStream.WriteAsync(new PrimeNumberDecompositionResponse
+                {
+                    Message = "Decomposed: " + k
+                });
+
+                N /= k;
+            } else
+            {
+                k += 1;
+            }
+        }
+
+    
+    }
+}
